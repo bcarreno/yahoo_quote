5e0136434da31cdc55282f2ea3f54a8a422fa4bf
Braulio Carreno <bcarreno@yahoo.com>
2012-02-17 13:57:30 -0500

    Add license and readme

--- test/yahoo_quote_test.rb
+++ test/yahoo_quote_test.rb
@@ -4,55 +4,65 @@ require 'yahoo_quote'
 require 'minitest/autorun'
 require 'fakeweb'
 
-# test error:    YahooQuote::Configuration.cache_dir = "/etc/kk"
-#
+# TODO test 2 things: make intermediate dirs for cache and permissions
+
 class TestYahooQuote < MiniTest::Unit::TestCase
   def setup
+    @url_regexp = %r(http://download\.finance\.yahoo\.com/d/quotes\.csv\?)
+    FakeWeb.allow_net_connect = false
+    FakeWeb.register_uri(:get, @url_regexp, :response => File.read("test/fakeweb/aapl_good.csv"))
+
     # TODO make gem method to clear the cache
     `rm -f /tmp/*csv`
   end
 
-  def test_live_quote
-    quote = YahooQuote::Quote.new('CSCO', ['Symbol', 'Name'])
-    assert_equal "CSCO",              quote.data["Symbol"]
-    assert_equal "Cisco Systems, In", quote.data["Name"]
-  end
-
-  def test_invalid_ticker_symbol
-    quote = YahooQuote::Quote.new('ECOMMERCE', ['Symbol', 'Name', 'Market Capitalization'])
-    assert_nil quote.data["Name"]
+  def test_empty_arguments
+    quote = YahooQuote::Quote.new('', [])
     assert_equal false, quote.valid?
-    # TODO check no cached
+    assert_nil quote.data["Symbol"]
+    assert_nil quote.data["Name"]
   end
 
-  def test_get_quote
-    quote = YahooQuote::Quote.new('AAPL', ['Symbol', 'Name', 'Last Trade (Price Only)', 'Market Capitalization'])
-    FakeWeb.register_uri(:get, quote.quote_url, :response => File.read('test/fakeweb/aapl.csv'))
+  def test_bad_symbol
+    quote = YahooQuote::Quote.new('C A', ['Symbol', 'Name'])
+    assert_equal false, quote.valid?
+    assert_nil quote.data["Symbol"]
+    assert_nil quote.data["Name"]
+  end
+
+  def test_non_existing_company
+    FakeWeb.register_uri(:get, @url_regexp, :response => File.read("test/fakeweb/ecommerce.csv"))
+    quote = YahooQuote::Quote.new('ECOMMERCE', ['Symbol', 'Name'])
+    assert_equal false, quote.valid?
+    assert_nil quote.data["Symbol"]
+    assert_nil quote.data["Name"]
+  end
+
+  def test_good_quote
+    quote = YahooQuote::Quote.new('AAPL', ['Name', 'Last Trade (Price Only)', 'P/E Ratio'])
     assert_equal "Apple Inc.", quote.data["Name"]
-    assert_equal 463.97,       quote.data["Last Trade (Price Only)"].to_f
-    assert_equal '432.6B',     quote.data["Market Capitalization"]
+    assert_equal 503.65,       quote.data["Last Trade (Price Only)"]
+    assert_equal 14.16,        quote.data["P/E Ratio"]
   end
 
   def test_graph_url
-    quote = YahooQuote::Quote.new('AAPL', ['Symbol', 'Name', 'Last Trade (Price Only)', 'Market Capitalization'])
-    FakeWeb.register_uri(:get, quote.quote_url, :response => File.read('test/fakeweb/aapl.csv'))
-    # TODO this should not be necessary
-    assert_equal "Apple Inc.", quote.data["Name"]
+    quote = YahooQuote::Quote.new('AAPL', [])
+    assert quote.valid?
     assert_match %r(^http://chart.finance.yahoo.com/z\?s=AAPL), quote.graph_url
   end
 
   def test_get_quote_from_cache
-    YahooQuote::Configuration.cache_dir = "/tmp"
-    quote = YahooQuote::Quote.new('AAPL', ['Symbol', 'Name', 'Last Trade (Price Only)'])
-    FakeWeb.register_uri(:get, quote.quote_url, :response => File.read('test/fakeweb/aapl.csv'))
+    YahooQuote::Configuration.cache_dir = "test/cache"
+    quote = YahooQuote::Quote.new('AAPL', ['Name', 'Last Trade (Price Only)', 'P/E Ratio'])
+    assert File.file? "test/cache/AAPL.csv"
     assert_equal "Apple Inc.", quote.data["Name"]
-    assert_equal 463.97,       quote.data["Last Trade (Price Only)"].to_f
-    # the quote should be cached now
-    quote2 = YahooQuote::Quote.new('AAPL', ['Symbol', 'Name', 'Last Trade (Price Only)'])
-    response = File.read('test/fakeweb/aapl_bad.csv')
-    refute_match /Apple/i, response
-    FakeWeb.register_uri(:get, quote2.quote_url, :response => response)
-    assert_equal "Apple Inc.", quote2.data["Name"]
-    assert_equal 463.97,       quote2.data["Last Trade (Price Only)"].to_f
+    assert_equal 503.65,       quote.data["Last Trade (Price Only)"]
+    bad_response = File.read('test/fakeweb/aapl_bad.csv')
+    refute_match /Apple/i, bad_response
+    FakeWeb.register_uri(:get, @url_regexp, :response => bad_response)
+    cached_quote = YahooQuote::Quote.new('AAPL', ['Name', 'Last Trade (Price Only)', 'P/E Ratio'])
+    assert_equal "Apple Inc.", cached_quote.data["Name"]
+    assert_equal 503.65,       cached_quote.data["Last Trade (Price Only)"]
+    YahooQuote::Configuration.cache_dir = nil
   end
 end
