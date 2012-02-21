8c6acdfd2bf9b42984010193666b805580c54f94
Braulio Carreno <bcarreno@yahoo.com>
2012-02-21 16:33:31 -0500

    Fix case path = nil

--- lib/yahoo_quote/configuration.rb
+++ lib/yahoo_quote/configuration.rb
@@ -2,9 +2,13 @@ module YahooQuote
   class Configuration
 
     def self.cache_dir=(path)
-      dir = path.to_s
-      Dir.mkdir(dir) unless dir.empty? || File.directory?(dir)
-      @@cache_dir = dir
+      if !path
+        @@cache_dir = path
+      else
+        dir = path.to_s
+        Dir.mkdir(dir) unless dir.empty? || File.directory?(dir)
+        @@cache_dir = dir
+      end
     end
 
     def self.cache_dir
